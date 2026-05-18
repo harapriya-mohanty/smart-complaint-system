8b313dafa51d7d6de9e5e39ea5dff303c2469b48
Harapriya <harapriyamohanty270@gmail.com>
2026-05-18 20:02:18 +0530

    harcoded api production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://smart-complaint-system-kx1p.onrender.com/api",
 });
 
 // Attach token automatically to every request
